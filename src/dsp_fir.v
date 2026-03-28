23630a9aa2e51f6ea6450eb08ba3224ad92af76f
seapanda0 <1211112330@student.mmu.edu.my>
2026-03-28 10:59:12 +0800

    added full implementation but placement fails

--- src/dsp_fir.v
+++ src/dsp_fir.v
@@ -51,6 +51,7 @@ module dsp_fir(
     //     end
     // end
 
+    // FILTER COEFFICIENTS
     // Register for 8 x 8 bit filter coefficients 
     reg signed [7:0] fir_coeff [0:7];
 
@@ -62,7 +63,7 @@ module dsp_fir(
                 fir_coeff[i] <= 8'b0;
             end
         end
-        else if (mode) begin
+        else if (mode == 1'b1) begin
             fir_coeff[0] <= data_in;
             fir_coeff[1] <= fir_coeff[0];
             fir_coeff[2] <= fir_coeff[1];
@@ -74,7 +75,55 @@ module dsp_fir(
         end
     end
 
+    // DATA PIPELINE & SHIFTING
+    reg signed [7:0] data_pipeline[0:7]; //input data shift register
+    integer  j;
+    always @(posedge clk) begin 
+        if(!rst_n) begin 
+            for (j = 0; j < 8; j = j + 1) begin
+                data_pipeline[j] <= 8'sd0;
+            end
+        end else if (mode == 1'b0) begin //only shift data when in run mode
+            data_pipeline[0] <= data_in;
+            data_pipeline[1] <= data_pipeline[0];
+            data_pipeline[2] <= data_pipeline[1];
+            data_pipeline[3] <= data_pipeline[2];
+            data_pipeline[4] <= data_pipeline[3];
+            data_pipeline[5] <= data_pipeline[4];
+            data_pipeline[6] <= data_pipeline[5];
+            data_pipeline[7] <= data_pipeline[6];
+        end
+    end
+
+    // DSP FILTER
+    reg signed [15:0] mult_reg [0:7]; //multiplying 8bit * 8bit gives a 16 bit result
+    integer  k;
+    integer  l;
+    always @(posedge clk)begin 
+        if (!rst_n)begin 
+            for (k = 0; k < 8; k = k + 1) begin
+                mult_reg[k] <= 16'sd0;
+            end
+        end else if (mode==1'b0)begin //perform multiplication and store in a register immediately
+            for (l = 0; l < 8; l = l + 1) begin
+                mult_reg[l] <= data_pipeline[l] * fir_coeff[l]; 
+            end
+        end
+    end
+    
+    wire signed [18:0] sum; //summing eight 16-bit numbers requires 3 extra bits to prevent overflow
+    
+    assign sum = mult_reg[0] + mult_reg[1] + 
+                 mult_reg[2] + mult_reg[3] + 
+                 mult_reg[4] + mult_reg[5] + 
+                 mult_reg[6] + mult_reg[7];
+                 
+    //scale the output, drop the lower 8 bits and take the next 8 bits
+    
+    assign data_out=sum[15:8];
+
+    // TEMPORARY
     // Prevent registers from be optimized out
-    assign data_out = fir_coeff[data_in[2:0]];
+    // assign data_out = fir_coeff[data_in[2:0]];
 
 endmodule
